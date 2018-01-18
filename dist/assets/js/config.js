e627ca402e288133be6639e645c5fcfedaff9e37
Urist McDwarf <umcdwarf@gmail.com>
2018-01-18 12:27:18 +0600

    Add missing config

---
+++ dist/assets/js/config.js
@@ -0,0 +1,22 @@
+var config = {
+    eth: {
+        fiatExchangeRate: 679.01
+    },
+    token: {
+        price: 0.000092,
+        bonus: 0.4,
+        minTokenAmount:0,
+        maxTokenAmount:25000000/1.4,
+        saleProgress: 0.000753,
+        saleAmount: 1.23456
+    },
+    messages: {
+        errors: {
+            validation: {
+                invalidWallet: 'Invalid wallet',
+                invalidPhone: 'Phone is invalid',
+                requestFailed: 'Validation failed (Request failed)'
+            }
+        }
+    }
+};
